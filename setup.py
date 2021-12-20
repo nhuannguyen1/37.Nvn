fdb27b254dd70f6b57ae9e0af37954cd3902b110
Nguyen Van Nhuan <nhuannv.vs@gmail.com>
2021-12-20 18:28:36 +0700

    all

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-DESCRIPTION = 'python for excel'
+DESCRIPTION = 'python cho người đã đi làm hoặc là cho dân tay ngang'
 
 # Setting up
 setup(
-    name="ppyan",
+    name="tpyan",
     version="0.0.0",
     author="Nguyen Van Nhuan",
     author_email="<nhuannv.vs@gmail.com>",
@@ -19,8 +19,8 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['pyautogui', 'pyaudio'],
-    keywords=['python', 'python excel'],
+    install_requires=['pandas', 'xlwings'],
+    keywords=['python', 'python excel', "pyan","pyexcel"],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
